947c220f002798b14f1d58de3637383107c75c5a
ravirun <94140075+ravirun@users.noreply.github.com>
2025-09-12 19:41:13 +0530

    Update package dependencies, add Inngest integration for invoking functions, and refactor homepage layout to include a button for triggering Inngest functions.

---
+++ inngest/functions.ts
@@ -0,0 +1,13 @@
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    // create a blueprints table in the database
+    await step.sleep("wait-a-moment", "10s");
+    await step.sleep("wait-a-moment", "30s");
+    await step.sleep("wait-a-moment", "5s");
+    return { message: `Hello ${event.data.email}!` };
+  },
+);

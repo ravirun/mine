b746a1e0ccb8c530e7aa5fb353b5d21a8a27224a
ravirun <94140075+ravirun@users.noreply.github.com>
2025-09-12 14:49:57 +0530

    Add button component to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+  
 
 export default function Home() {
   return (
    <div>
     Mine Ai Website Builder
+    <Button>Click me</Button>
    </div>
   );
 }

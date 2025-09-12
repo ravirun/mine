05891d0d7b64253c09235f60e79ebcd590de4391
ravirun <94140075+ravirun@users.noreply.github.com>
2025-09-13 04:14:38 +0530

    Update package dependencies to include @e2b/code-interpreter v2.0.0, and enhance Inngest function to utilize sandbox creation and retrieval for dynamic code execution.

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,9 +1,16 @@
 import { inngest } from "./client";
+import { getSandboxId } from "./utils";
+import { Sandbox } from "@e2b/code-interpreter";  
 import { createAgent, openai } from '@inngest/agent-kit';
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event,step }) => {
+     const sandboxid = await step.run('get-sandbox-id', async () => {
+      const sandbox = await Sandbox.create('mine-nextjs-test-2')
+      return sandbox.sandboxId
+    })
+
     const codeAgent = createAgent({
       name: "code-agent",
       system: "You are a expert next.js developer that writes readable, maintainable, and efficient code. You write simple write simple next.js and react snippets.",
@@ -15,6 +22,12 @@ export const helloWorld = inngest.createFunction(
     const { output } = await codeAgent.run(
             `write a simple next.js and react snippet that ${event.data.value}`,
       );
-    return {output};
+
+      const sandboxUrl = await step.run('get-sandbox-url', async () => {
+        const sandbox = await getSandboxId(sandboxid)
+        const host = await sandbox.getHost(3000)
+        return `https://${host}`
+      })
+    return {output, sandboxUrl};
   },
 );

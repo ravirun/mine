afcc8f7494177095b539b4cd2303dbaa6b8d341e
ravirun <94140075+ravirun@users.noreply.github.com>
2025-10-23 23:47:45 +0530

    fix: add missing UI components from 10-message-ui branch

---
+++ modules/projects/server/UI/view/components/Message-card.tsx
@@ -0,0 +1,116 @@
+import { Badge } from "@/components/ui/badge"
+import { Fragment, MessageRole, MessageType } from "@/lib/generated/prisma"
+import { Card, CardContent } from "@/components/ui/card"
+import { cn } from "@/lib/utils"
+import { format } from "date-fns"
+import Image from "next/image"
+import { ChevronRightIcon, Code2Icon } from "lucide-react"
+
+interface UserMessageProps {
+    content: string
+}
+
+const UserMessage = ({ content }: UserMessageProps) => {
+    return (
+        <div className="flex justify-end pb-4 pl-10">
+            <Card className="rounded-lg bg-muted p-3 shadow-none border-none max-w-[80%] break-words">
+
+                {content}
+
+            </Card>
+        </div>
+    )
+}
+
+interface FragmentCardProps {
+    fragment: Fragment
+    isActiveFragment: boolean
+    onFragmentClick: (fragment: Fragment) => void
+}
+
+const FragmentCard = ({ fragment, isActiveFragment, onFragmentClick }: FragmentCardProps) => {
+    return (
+        <button className={cn("flex items-start text-start gap-2 border rounded-lg bg-muted w-fit p-3 hover:bg-secondary transition-colors", isActiveFragment && "bg-primary text-primary-foreground border-primary hover:bg-primary")}
+            onClick={() => onFragmentClick(fragment)}>
+            <Code2Icon className="size-4 mt-0.5 shrink-0" /> 
+            <div className="flex flex-col gap-y-1">
+                <span className="text-sm font-medium line-clamp-1" >
+                    {fragment.title}
+                </span>
+                <span className="text-sm">Preview</span>
+            </div>
+            <div>
+                <ChevronRightIcon className="size-4 mt-0.5 shrink-0" />
+            </div>
+        </button>
+    )
+}
+interface AssistantMessageProps {
+    content: string
+    fragment: Fragment | null
+    createdAt: Date
+    isActiveFragment: boolean
+    onFragmentClick: (fragment: Fragment) => void
+    type: MessageType
+}
+const AssistantMessage = ({ content, fragment, createdAt, isActiveFragment, onFragmentClick, type }: AssistantMessageProps) => {
+    return (
+        <div className={cn(
+            "flex flex-col group px-2 pb-4",
+            type === "ERROR" && "text-destructive"
+        )}>
+            <div className="flex items-center gap-2 pl-2 mb-2">
+                {/* todo add logo */}
+                <Image src="/logo.svg" alt="Mine" width={24} height={24} />
+                <span className="text-sm font-medium" >
+                    Mine
+                </span>
+                <span className="text-sm text-muted-foreground opacity-0 group-hover:opacity-100 transition-opacity" >
+                    {format(createdAt, "HH:mm 'on' MMM dd, yyyy")}
+                </span>
+
+            </div>
+            <div className="pl-8.5 flex flex-col gap-y-4">
+                {content}
+                {fragment && (
+                    <FragmentCard
+                        fragment={fragment}
+                        isActiveFragment={isActiveFragment}
+                        onFragmentClick={onFragmentClick}
+                    />
+                )}
+            </div>
+
+        </div>
+    )
+}
+
+interface Props {
+    content: string
+    role: MessageRole
+    fragment: Fragment | null
+    createdAt: Date
+    isActiveFragment: boolean
+    onFragmentClick: (fragment: Fragment) => void
+    type: MessageType
+}
+
+
+export const MessageCard = ({ content, role, fragment, createdAt, isActiveFragment, onFragmentClick, type }: Props) => {
+    if (role === MessageRole.ASSISTANT) {
+        return (
+            <AssistantMessage
+                content={content}
+                fragment={fragment}
+                createdAt={createdAt}
+                isActiveFragment={isActiveFragment}
+                onFragmentClick={onFragmentClick}
+                type={type}
+            />
+        )
+
+    }
+    return (
+        <UserMessage content={content} />
+    )
+}

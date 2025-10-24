34eabbf6191452287d0603c3b1600cf46b909a29
ravirun <94140075+ravirun@users.noreply.github.com>
2025-10-24 13:56:06 +0530

    feat: enhance project view and messaging components
    - Integrated ThemeProvider for theme management in the layout.
    - Added ProjectHeader component for project-specific UI with theme toggle.
    - Updated MessagesContainer to handle active fragments and display loading states.
    - Introduced MessageLoading component for improved user feedback during message loading.
    - Refactored MessageForm to ensure proper message creation and query invalidation.

--- modules/projects/server/UI/view/project-view.tsx
+++ modules/projects/server/UI/view/project-view.tsx
@@ -5,20 +5,27 @@ import { useSuspenseQuery } from "@tanstack/react-query"
 import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "@/components/ui/resizable"
 import { MessagesContainer } from "@/modules/projects/server/UI/view/components/MessagesContainer"
 import { Suspense } from "react"
+import { Fragment } from "@/lib/generated/prisma"
+import { useState } from "react"
+import { ProjectHeader } from "@/modules/projects/server/UI/view/components/project-header"
 
 
 interface ProjectViewProps {
     projectId: string
 }
 export const ProjectView = ({ projectId }: ProjectViewProps) => {
+    const [activeFragment, setActiveFragment] = useState<Fragment | null>(null)
 
 
     return (
         <div className="h-screen">
             <ResizablePanelGroup direction="horizontal">
                 <ResizablePanel defaultSize={35} minSize={20} className="flex flex-col min-h-0">
+                    <Suspense fallback={<div>Loading project...</div>}>
+                    <ProjectHeader projectId={projectId} />
+                    </Suspense>
                     <Suspense fallback={<div>Loading messages...</div>}>
-                        <MessagesContainer projectId={projectId} />
+                        <MessagesContainer projectId={projectId} activeFragment={activeFragment} setActiveFragment={setActiveFragment} />
                     </Suspense>
                 </ResizablePanel>
                 <ResizableHandle withHandle />
